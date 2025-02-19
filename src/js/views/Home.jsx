5704ba212132c5211595ead18acf7a169208210c
Miguel Toyas Pernichi <mitoperni@gmail.com>
2025-02-19 16:09:29 +0000

    Rollback to todolist sin fetch

--- src/js/views/Home.jsx
+++ src/js/views/Home.jsx
@@ -5,107 +5,30 @@ import TaskList from "../components/TaskList";
 const Home = () => {
   const [tasks, setTasks] = useState([]);
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const fetchTodos = async () => {
-    try {
-      const response = await fetch(
-        "https://playground.4geeks.com/todo/users/mitoperni"
-      );
-
-      if (!response.ok) {
-        throw new Error("¡Vaya! No hemos podido obtener las tareas");
-      }
-
-      const data = await response.json();
-
-      setTasks(data.todos);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const addTasks = async (label) => {
+  const addTasks = (label) => {
     const newTask = {
+      id: Date.now(),
       label,
       is_done: false,
     };
 
-    try {
-      const response = await fetch(
-        "https://playground.4geeks.com/todo/todos/mitoperni",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newTask),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Error al crear la tarea");
-      }
-
-	  const data = await response.json()
-
-      setTasks([...tasks, data]);
-    } catch (error) {
-      console.log(error);
-    }
+    setTasks([...tasks, newTask]);
   };
 
-  const deleteTask = async (id) => {
-    try {
-      const response = await fetch(
-        `https://playground.4geeks.com/todo/todos/${id}`,
-        {
-          method: "DELETE", // 👈 Especificamos que es DELETE
-        }
-      );
-
-      if (!response.ok) throw new Error("Error al borrar la tarea");
-
-      setTasks(tasks.filter((task) => task.id !== id));
-    } catch (error) {
-      console.log(error);
-    }
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
   };
 
-  const toggleTaskComplete = async (id) => {
-    const taskToUpdate = tasks.find((task) => task.id === id);
-
-    try {
-      const response = await fetch(
-        `https://playground.4geeks.com/todo/todos/${id}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            ...taskToUpdate,
-            is_done: !taskToUpdate.is_done,
-          }),
+  const toggleTaskComplete = (id) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, is_done: !task.is_done };
+        } else {
+          return task;
         }
-      );
-
-      if (!response.ok) throw new Error("Error al actualizar la tarea");
-
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === id) {
-            return { ...task, is_done: !task.is_done };
-          } else {
-            return task;
-          }
-        })
-      );
-    } catch (error) {
-      console.log(error);
-    }
+      })
+    );
   };
 
   return (
